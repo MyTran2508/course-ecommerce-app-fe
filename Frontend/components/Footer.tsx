b0e450e7d06c290af204ed50db586ef54587f7e1
phamngocthang <20110728@student.hcmute.edu.vn>
2023-10-23 23:32:19 +0700

    FE - install sass

--- Frontend/components/Footer.tsx
+++ Frontend/components/Footer.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import './css/footer.css'
+import './css/footer.scss'
 import ReactDOM from 'react-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faFacebook, faInstagram, faLinkedinIn, faTwitter, faYoutube, fab } from '@fortawesome/free-brands-svg-icons'
@@ -22,11 +22,11 @@ function Footer() {
     <footer>
       <div className="content">
         <div className="top">
-          <div className="logo-details">
-            <div className="logo"><FontAwesomeIcon icon="fab fa-slack" /></div>
-            <span className="logo_name">E-LEARNING</span>
+          <div className="top__logo">
+            <div className="image"><FontAwesomeIcon icon="fab fa-slack" /></div>
+            <span>E-LEARNING</span>
           </div>
-          <div className="media-icons">
+          <div className="top__media">
             <Link href={"/"}><div className="icon facebook"><FontAwesomeIcon icon="fab fa-facebook" /></div></Link>
             <Link href={"/"}><div className="icon twitter"><FontAwesomeIcon icon="fa-brands fa-twitter" /></div></Link>
             <Link href={"/"}><div className="icon instagram"><FontAwesomeIcon icon="fa-brands fa-instagram" /></div></Link>
@@ -34,37 +34,37 @@ function Footer() {
             <Link href={"/"}><div className="icon youtube"><FontAwesomeIcon icon="fa-brands fa-youtube" /></div></Link>
           </div>
         </div>
-        <div className="link-boxes">
-          <ul className="box">
-            <li className="link-name">Company</li>
+        <div className="body">
+          <ul className="body__box">
+            <li>Company</li>
             <li><Link href={"/"}>Home</Link></li>
             <li><Link href={"/"}>Contact us</Link></li>
             <li><Link href={"/"}>About us</Link></li>
             <li><Link href={"/"}>Get started</Link></li>
           </ul>
-          <ul className="box">
-            <li className="link-name">Courses</li>
+          <ul className="body__box">
+            <li>Courses</li>
             <li><Link href={"/"}>Game Development</Link></li>
             <li><Link href={"/"}>Web Development</Link></li>
             <li><Link href={"/"}>Mobile Development</Link></li>
             <li><Link href={"/"}>Software Testing</Link></li>
           </ul>
-          <ul className="box">
-            <li className="link-name">Account</li>
+          <ul className="body__box">
+            <li>Account</li>
             <li><Link href={"/"}>Profile</Link></li>
             <li><Link href={"/"}>My account</Link></li>
             <li><Link href={"/"}>Shopping cart</Link></li>
             <li><Link href={"/"}>Change password</Link></li>
           </ul>
-          <ul className="box box-input">
-            <li className="link-name">Subscribe</li>
+          <ul className="body__box body__box--input">
+            <li>Subscribe</li>
             <li><input type="text" placeholder="Enter your email"/></li>
             <li><button>Subscribe</button></li>
           </ul>
         </div>
       </div>
       <div className="policy">
-        <div className="policy-details">
+        <div className="policy__details">
           <span>Copy right @ 2023 | ALL Right Reserved</span>
           <Link href={"/"}>Privacy policy</Link>
           <Link href={"/"}>Terms & condition</Link>
