fac2c508366146e2a293cc353e65b99127e691d8
MyTran2508 <mytranchi2508@mail.com>
2023-11-02 10:06:08 +0700

    BE-AddCORSMapping

--- Backend/user-service/src/main/java/com/programming/userservice/domain/dto/UserDto.java
+++ Backend/user-service/src/main/java/com/programming/userservice/domain/dto/UserDto.java
@@ -12,7 +12,8 @@ import java.util.Set;
 @AllArgsConstructor
 @ToString
 public class UserDto {
-    @NotEmpty(message = "Password is required")
+    private String id;
+//    @NotEmpty(message = "Password is required")
     @Size(min = 2, max = 32, message = "Password must be between 2 and 32 characters")
     private String password;
     @Email(message = "Invalid email address")
--- Backend/user-service/src/main/java/com/programming/userservice/security/config/AuthConfig.java
+++ Backend/user-service/src/main/java/com/programming/userservice/security/config/AuthConfig.java
@@ -4,6 +4,7 @@ import com.programming.userservice.security.jwt.JwtAuthFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -18,12 +19,29 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AuthConfig {
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedMethods(Collections.singletonList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
     @Bean
     public UserDetailsService userDetailsService() {
         return new CustomUserDetailsService();
@@ -41,6 +59,7 @@ public class AuthConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/users/user/login",
                                         "/api/users/user/register/**",
                                         "/api/users/user/forget-password/**",
