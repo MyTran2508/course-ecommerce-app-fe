51fe3add6bbd6822849a52836d9fbfcc1832d130
MyTran2508 <mytranchi2508@mail.com>
2024-01-27 09:04:41 +0700

    FE - update InputEditor

---
+++ Frontend/app/test/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import InputEditor from "../instructor/courses/[id]/manage/InputEditor";
+
+function Page() {
+  return (
+    <div className="w-2/3 flex justify-center">
+      <InputEditor />
+    </div>
+  );
+}
+
+export default Page;
