000d799e187e7453f7ff19e74c529418e53e54f4
Nguyen Thanh Phuong Thao <mytranchi2508@gmail.com>
2024-04-28 22:44:00 +0700

    [BE] CRS - 20: Convert loadFile to base64

--- Backend/user-service/src/main/java/com/programming/userservice/utilities/constant/SecurityConstrant.java
+++ Backend/user-service/src/main/java/com/programming/userservice/utilities/constant/SecurityConstrant.java
@@ -8,5 +8,5 @@ public class SecurityConstrant {
 
     public static final String tokenPrefix = "Bearer ";
 
-    public static final int tokenExpirationAfterDays = 30 * 24 * 60 * 60 * 1000; // 30 day
+    public static final int tokenExpirationAfterDays = 1 * 24 * 60 * 60 * 1000; // 30 day
 }
