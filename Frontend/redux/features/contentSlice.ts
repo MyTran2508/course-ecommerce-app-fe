e45013691c1472adbd653949efe60d6666101cef
Nguyen Thanh Phuong Thao <mytranchi2508@gmail.com>
2024-06-24 21:32:01 +0700

    [FE] CRS - 14: add assignmentHistory API

--- Frontend/redux/features/contentSlice.ts
+++ Frontend/redux/features/contentSlice.ts
@@ -1,24 +1,36 @@
+import { Assignment } from "@/types/assignment.type";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-
+import { set } from "lodash";
 
 export interface ContentState {
-  contentId: string
+  contentId: string;
+  lectureIdForAssignment: string;
+  assignment: Assignment | null;
 }
 
 const initialState: ContentState = {
-  contentId: ""
-}
+  contentId: "",
+  lectureIdForAssignment: "",
+  assignment: null,
+};
 
 export const content = createSlice({
   name: "content",
   initialState,
   reducers: {
     setContentId: (state, action: PayloadAction<string>) => {
-      state.contentId = action.payload
+      state.contentId = action.payload;
+    },
+    setLectureIdForAssignment: (state, action: PayloadAction<string>) => {
+      state.lectureIdForAssignment = action.payload;
+    },
+    setAssignment: (state, action: PayloadAction<Assignment | null>) => {
+      state.assignment = action.payload;
     },
   },
 });
 
-export const {setContentId} = content.actions;
+export const { setContentId, setLectureIdForAssignment, setAssignment } =
+  content.actions;
 
 export default content.reducer;
