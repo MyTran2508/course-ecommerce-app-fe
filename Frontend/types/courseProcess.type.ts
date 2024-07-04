1dbf99a337eb7c9276976cb8b244f3208d948979
Nguyen Thanh Phuong Thao <mytranchi2508@gmail.com>
2024-07-04 07:48:15 +0700

    update

--- Frontend/types/courseProcess.type.ts
+++
@@ -1,9 +0,0 @@
-import { Course } from "./course.type"
-
-export interface CourseProcess {
-    userId: string
-    currentProgress: number
-    totalAmountOfLecture: number
-    rateProgress: number
-    course: Course
-}
--- Frontend/utils/regex.ts
+++
@@ -1,5 +0,0 @@
-export const phoneNumberRegExp = /^0\d{9}$/;
-
-export const urlRegex = /^https:\/\//;
-
-export const newLineRegex = /\/n/;
